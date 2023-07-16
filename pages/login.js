226a1c6ffca641f28f62994c11ff1a80f1086192
James Korin <jameskorin@Jamess-MacBook-Air.local>
2023-07-16 13:44:44 -0400

    ugly sign up and login screens

---
+++ pages/login.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { useRouter } from 'next/router'
+
+export default function Login() {
+    
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const router = useRouter();
+
+    const login =async ()=> {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+        if(error === null)
+            router.push('/home');
+        else
+            setErrorMessage(error.toString().substring('AuthAPIError: '.length));
+    }
+
+    return <div>
+        <div>Log In</div>
+        <form onSubmit={e => {
+            e.preventDefault();
+            login();
+        }}>
+            <input placeholder='Email' onChange={e => setEmail(e.target.value)} type='email'/>
+            <input placeholder='Password' onChange={e => setPassword(e.target.value)} type='password'/>
+            <button type='submit'>Submit</button>
+        </form>
+        <div>{errorMessage}</div>
+    </div>
+}
---
+++ pages/signup.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { useRouter } from 'next/router'
+
+export default function Signup() {
+    
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const router = useRouter();
+
+    const signup =async ()=> {
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+        if(error === null)
+            router.push('/home');
+        else
+            setErrorMessage(error.toString().substring('AuthAPIError: '.length));
+    }
+
+    return <div>
+        <div>Sign Up</div>
+        <form onSubmit={e => {
+            e.preventDefault();
+            signup();
+        }}>
+            <input placeholder='Email' onChange={e => setEmail(e.target.value)} type='email'/>
+            <input placeholder='Password' onChange={e => setPassword(e.target.value)} type='password'/>
+            <button type='submit'>Submit</button>
+        </form>
+        <div>{errorMessage}</div>
+    </div>
+}
