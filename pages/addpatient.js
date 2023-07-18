44e2e5460661073fe5244dc53800ff5eb13619e5
James Korin <jameskorin@Jamess-MacBook-Air.local>
2023-07-18 08:48:45 -0400

    select state from dropdown

--- pages/addpatient.js
+++ pages/addpatient.js
@@ -6,6 +6,7 @@ export default function AddPatient() {
     const [first, setFirst] = useState('');
     const [middle, setMiddle] = useState('');
     const [last, setLast] = useState('');
+    const [dob, setDob] = useState(new Date());
     const [addresses, setAddresses] = useState([{
         street: '',
         city: '',
@@ -14,11 +15,13 @@ export default function AddPatient() {
         primary: true
     }]);
 
-    useEffect(() => {
-        console.log(addresses);
-    },[addresses])
-
     const addPatient =async ()=> {
+        // Check if form is submittable
+            // first, middle, last names are not blank
+            // dob valid (any date in the past, for now)
+            // 
+
+
         // Add row to the patients table
         // Get id of the new row
         // Create new rows for each address referring to patient
@@ -69,7 +72,12 @@ export default function AddPatient() {
                 <div key={`address_${index}`}>
                     <input placeholder='Street' onChange={e => editAddress(index, 'street', e.target.value)} value={item.street}/>
                     <input placeholder='City' onChange={e => editAddress(index, 'city', e.target.value)} value={item.city}/>
-                    <input placeholder='State' onChange={e => editAddress(index, 'state', e.target.value)} value={item.state}/>
+                    <select defaultValue='' placeholder='State' onChange={e => editAddress(index, 'state', e.target.value)} value={item.state}>
+                        <option selected={item.state === ''} value='' disabled>State</option>
+                        {states.map((item_state, index_state) => (
+                            <option value={item_state} key={`${index}_${item_state}`}>{item_state}</option>
+                        ))}
+                    </select>
                     <input placeholder='Zip' onChange={e => editAddress(index, 'zip', e.target.value)} value={item.zip}/>
                     <input type='radio' name='primary_address' onClick={e => setPrimaryAddress(index)} checked={item.primary}/>
                     <button onClick={() => removeAddress(index)}>Remove Address</button>
@@ -82,3 +90,56 @@ export default function AddPatient() {
         </form>
     </div>
 }
+
+const states = [
+'Alabama',
+'Alaska',
+'Arizona',
+'Arkansas',
+'California',
+'Colorado',
+'Connecticut',
+'Delaware',
+'Florida',
+'Georgia',
+'Hawaii',
+'Idaho',
+'Illinois',
+'Indiana',
+'Iowa',
+'Kansas',
+'Kentucky',
+'Louisiana',
+'Maine',
+'Maryland',
+'Massachusetts',
+'Michigan',
+'Minnesota',
+'Mississippi',
+'Missouri',
+'Montana',
+'Nebraska',
+'Nevada',
+'New Hampshire',
+'New Jersey',
+'New Mexico',
+'New York',
+'North Carolina',
+'North Dakota',
+'Ohio',
+'Oklahoma',
+'Oregon',
+'Pennsylvania',
+'Rhode Island',
+'South Carolina',
+'South Dakota',
+'Tennessee',
+'Texas',
+'Utah',
+'Vermont',
+'Virginia',
+'Washington',
+'West Virginia',
+'Wisconsin',
+'Wyoming',
+];
