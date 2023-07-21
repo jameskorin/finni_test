2a2374aaea45b30186c9d73699d588f47dc195e1
James Korin <jameskorin@jamess-air.myfiosgateway.com>
2023-07-20 20:03:34 -0400

    moved states out to separate file

--- pages/addpatient.js
+++ pages/addpatient.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import _ from 'lodash'
+import { states } from '../util/states'
 import { createClient } from '@supabase/supabase-js'
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
 
@@ -211,56 +212,3 @@ export default function AddPatient({
         <div>{error}</div>
     </div>
 }
-
-const states = [
-'Alabama',
-'Alaska',
-'Arizona',
-'Arkansas',
-'California',
-'Colorado',
-'Connecticut',
-'Delaware',
-'Florida',
-'Georgia',
-'Hawaii',
-'Idaho',
-'Illinois',
-'Indiana',
-'Iowa',
-'Kansas',
-'Kentucky',
-'Louisiana',
-'Maine',
-'Maryland',
-'Massachusetts',
-'Michigan',
-'Minnesota',
-'Mississippi',
-'Missouri',
-'Montana',
-'Nebraska',
-'Nevada',
-'New Hampshire',
-'New Jersey',
-'New Mexico',
-'New York',
-'North Carolina',
-'North Dakota',
-'Ohio',
-'Oklahoma',
-'Oregon',
-'Pennsylvania',
-'Rhode Island',
-'South Carolina',
-'South Dakota',
-'Tennessee',
-'Texas',
-'Utah',
-'Vermont',
-'Virginia',
-'Washington',
-'West Virginia',
-'Wisconsin',
-'Wyoming',
-];
